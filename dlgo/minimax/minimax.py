abbd4de8382decf3057c7bd4e4fa249bd991b31c
SeiichiN <billie175@gmail.com>
2020-03-09 12:28:55 +0900

    深さで刈り込まれたminimax探索はこんなところで

--- dlgo/minimax/minimax.py
+++ dlgo/minimax/minimax.py
@@ -18,14 +18,10 @@ def eval_situation(game_state):
 # 深さで枝刈りされたミニマックス検索    
 def best_result(game_state, max_depth, eval_fn):
     if game_state.is_over():
-        if game_state.winner() == game_state.next_player and max_depth == 2:
-            return MAX_SCORE + 10
-        elif game_state.winner() == game_state.next_player:
-            return MAX_SCORE
-        elif game_state.winner() == game_state.next_player.other and max_depth == 3:
-            return MIN_SCORE - 10
+        if game_state.winner() == game_state.next_player:
+            return MAX_SCORE + max_depth
         elif game_state.winner() == game_state.next_player.other:
-            return MIN_SCORE
+            return MIN_SCORE - max_depth
         else:
             return 0
 
@@ -48,56 +44,44 @@ def print_best_moves(str, best_moves):
 
 class MinimaxAgent(Agent):
     def select_move(self, game_state):
-        high_moves = []
         best_moves = []
-        better_moves = []
-        other_moves = []
-        low_moves = []
+        our_best_outcome = None
         for possible_move in game_state.legal_moves():
             print('possible_move %s %s' % (possible_move.point, game_state.next_player))
             next_state = game_state.apply_move(possible_move)        # <1>
             opponent_best_outcome = best_result(next_state, DEPTH, eval_situation)   # <2>
-            our_best_outcome = -1 * opponent_best_outcome            # <3>
-            print('our_best_outcome', our_best_outcome)
-            if our_best_outcome > MAX_SCORE:                        # <4>
-                high_moves.append(possible_move)
-            elif our_best_outcome == MAX_SCORE:
+            our_better_outcome = -1 * opponent_best_outcome          # <3>
+            if our_best_outcome is None:                             # <4>
+                our_best_outcome = our_better_outcome
                 best_moves.append(possible_move)
-            elif our_best_outcome == 0:
-                better_moves.append(possible_move)
-            elif our_best_outcome == MIN_SCORE:
-                other_moves.append(possible_move)
-            elif our_best_outcome < MIN_SCORE:
-                low_moves.append(possible_move)
+            elif our_best_outcome == our_better_outcome:               # <5>
+                best_moves.append(possible_move)
+            elif our_best_outcome < our_better_outcome:                # <6>
+                our_best_outcome = our_better_outcome
+                best_moves.clear()
+                best_moves.append(possible_move)
             else:
-                print('=================!')                          # <5>
-        if high_moves:
-            print_best_moves('high', high_moves)
-            return random.choice(high_moves)
+                print('負ける手なので、考慮の対象外')
+            print('our_better_outcome:%d  our_best_outcome:%d' % (our_better_outcome, our_best_outcome))
+
         if best_moves:
             print_best_moves('best', best_moves)
             return random.choice(best_moves)
-        if better_moves:
-            print_best_moves('better', better_moves)
-            return random.choice(better_moves)
-        if other_moves:
-            print_best_moves('other', other_moves)
-            return random.choice(other_moves)
-        if low_moves:
-            print_best_moves('low', low_moves)
-            return random.choice(low_moves)
+        else:
+            print('best_movesがないなんて、あり得ない...')
+            
     # <1> game_state.apply_move は、next_state を 相手側とする。
     #     つまり、possible_moveを適用した盤状況で、player は相手側であ
     #     る。
-    # <2> 探索の深さを 2 としてみた。
-    #     opponent_best_outcome -- 相手側が最善手を打った場合の値。
-    #                              MAX_SCORE, 0, MIN_SCORE のいずれか。
+    # <2> DEPTH -- 定数とした。
     # <3> 相手が MAX_SCORE なら、こちら側は MIN_SCORE である。
-    # <4> our_best_outcome が MAX_SCORE, 0, MIN_SCORE で処理を分ける。
-    #     MAX_SCOREの場合 -- best_moves のリストに着手を加える。
-    #     0               -- better_moves         〃
-    #     MIN_SCORE       -- other_moves          〃
-    # <5> それ以外は無いはずだけど。
+    # <4> our_best_outcome が None ならば、our_better_outcome の値を
+    #     our_best_outcome に入れて、best_moves にその着手を入れる。
+    # <5> our_best_outcome と our_better_outcome が同じなら
+    #     best_moves にその着手を追加する。
+    # <6> our_best_outcome よりも our_better_outcome の方が大きければ
+    #     best_moves を空にして、その着手をリストに入れる。
+
     
 # -----------------------------------
-# 修正時刻： Sat Mar  7 08:03:16 2020
+# 修正時刻： Mon Mar  9 11:23:42 2020
